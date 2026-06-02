85a6160172ea8e6f14e04a900a69621c3e17755d
Liam Fruzyna <liam@fruzyna.net>
2026-06-01 20:23:24 -0500

    Use provided nvidia-install file to fix nvidia build

--- build/01-akmods.sh
+++ build/01-akmods.sh
@@ -51,9 +51,7 @@ if [[ "$LIAMOS_IMAGE_NAME" =~ "nvidia" ]]; then
     fi
 
     # Install Nvidia RPMs
-    curl "https://raw.githubusercontent.com/ublue-os/main/main/build_files/nvidia-install.sh" -o /tmp/nvidia-install.sh
-    chmod +x /tmp/nvidia-install.sh
-    /tmp/nvidia-install.sh
+    /tmp/akmods-rpms/ublue-os/nvidia-install.sh
     rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
     ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
     tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<EOF
